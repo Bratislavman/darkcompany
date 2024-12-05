dbc2e8d81cc66f5cacbdfa90c19743549ff864f7
Bratislavman <kiro241@gmail.com>
2024-12-05 16:54:58 +0300

    проверка спрайта(движение, стоп)

--- game/character_body_2d.gd
+++ game/character_body_2d.gd
@@ -4,13 +4,27 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 var speed = SPEED
 
 func _physics_process(delta: float) -> void:
-	velocity.x = move_toward(velocity.x, speed, SPEED)
-	move_and_slide()
+	if (int(abs(get_global_mouse_position().x - position.x)) <= 5):
+		_animated_sprite.play("stay")
+	else:
+		velocity.x = move_toward(velocity.x, speed, SPEED)
+		move_and_slide()
+		if (get_global_mouse_position().x > position.x):
+			speed = SPEED
+			_animated_sprite.play("run")
+		else:
+			speed = SPEED*-1
+			_animated_sprite.play("run")
 
 	if (get_global_mouse_position().x > position.x):
-		speed = SPEED
+		_animated_sprite.flip_h = false
 	else:
-		speed = SPEED*-1
+		_animated_sprite.flip_h = true
+
+func _ready() -> void:
+	_animated_sprite.play("stay")
